8f79809f1e2e759a1c6a60ca305dc5c60674e116
akashsathe063 <akashsathe063@gmail.com>
2022-07-10 09:13:40 +0530

    Daily employee wage

--- src/com/blz/employee_wage/EmployeeWage.java
+++ src/com/blz/employee_wage/EmployeeWage.java
@@ -2,14 +2,19 @@ package com.blz.employee_wage;
 
 public class EmployeeWage {
     static int isPresent = 1;
+    static int wage_per_hour = 20;
+    static int full_day_hours = 8;
+    static int wage = 0;
 
     public static void main(String[] args) {
         System.out.println("Well come to Employee Wage computation");
         int checkRandom = (int) Math.floor(Math.random() * 10) % 2;
         if (checkRandom == isPresent) {
             System.out.println("Employee is present");
+            wage = wage_per_hour * full_day_hours;
         } else {
             System.out.println("Employee is Absent");
         }
+        System.out.println("Daily Employee Wage: " + wage);
     }
 }
