4428c04c67d8d7b88ee71cc703bfdc72614d3376
akashsathe063 <akashsathe063@gmail.com>
2022-07-10 10:50:50 +0530

    calculating Employee wage for multiple companies

--- src/com/blz/employee_wage/EmployeeWage.java
+++ src/com/blz/employee_wage/EmployeeWage.java
@@ -1,24 +1,36 @@
 package com.blz.employee_wage;
 
 public class EmployeeWage {
-    static int isPresent = 1;
-    static int isPartime_present = 2;
-    static int working_day_per_month = 20;
-    static int max_working_hours = 100;
-    static int wage_per_hour = 20;
-    static int full_day_hours = 8;
-    static int part_time_hours = 4;
-    static int wage = 0;
-    static int total_wage = 0;
-    static int total_hours = 0;
+    static final int isPresent = 1;
+    static final int isPartime_present = 2;
+    private String comapy_Name;
+    private  int max_working_hours;
+    private  int wage_per_hour;
+    private  int working_day_per_month;
+
+    public EmployeeWage(String comapy_Name, int max_working_hours, int wage_per_hour, int working_day_per_month) {
+        this.comapy_Name = comapy_Name;
+        this.max_working_hours = max_working_hours;
+        this.wage_per_hour = wage_per_hour;
+        this.working_day_per_month = working_day_per_month;
+    }
 
     public static void main(String[] args) {
         System.out.println("Well come to Employee Wage computation");
-        employee_Wage();
+        EmployeeWage company1 = new EmployeeWage("Google",50,30,25);
+        System.out.println("employee wage of company "+company1.comapy_Name+ " is: "+company1.employee_Wage());
+
+        EmployeeWage company2 =  new EmployeeWage("microsoft",80,25,30);
+        System.out.println("employee wage of company "+company2.comapy_Name+ " is: "+company2.employee_Wage());
     }
 
-    public static void employee_Wage() {
+    public  int employee_Wage() {
         int day = 0;
+        int total_hours = 0;
+        int full_day_hours = 8;
+        int part_time_hours = 4;
+        int wage = 0;
+        int  total_wage = 0;
         while (total_hours < max_working_hours && day <= working_day_per_month) {
             int checkRandom = (int) Math.floor(Math.random() * 10) % 3;
             switch (checkRandom) {
@@ -45,7 +57,7 @@ public class EmployeeWage {
             }
 
         }
-        System.out.println("Total Hours: " + total_hours);
-        System.out.println("Month Employee Wage: " + total_wage);
+
+        return total_wage;
     }
 }
