1a2313e57c4c4770e3d4f27af2624cc96ce3d4f4
akashsathe063 <akashsathe063@gmail.com>
2022-07-10 09:17:55 +0530

    part time employee and wage

--- src/com/blz/employee_wage/EmployeeWage.java
+++ src/com/blz/employee_wage/EmployeeWage.java
@@ -1,20 +1,26 @@
 package com.blz.employee_wage;
 
 public class EmployeeWage {
+
     static int isPresent = 1;
+    static int isPartime_present = 2;
     static int wage_per_hour = 20;
     static int full_day_hours = 8;
+    static int part_time_hours = 4;
     static int wage = 0;
 
     public static void main(String[] args) {
         System.out.println("Well come to Employee Wage computation");
-        int checkRandom = (int) Math.floor(Math.random() * 10) % 2;
+        int checkRandom = (int) Math.floor(Math.random() * 10) % 3;
         if (checkRandom == isPresent) {
             System.out.println("Employee is present");
             wage = wage_per_hour * full_day_hours;
+        } else if (checkRandom == isPartime_present) {
+            System.out.println("Employee is Part time Present");
+            wage = wage_per_hour * part_time_hours;
         } else {
             System.out.println("Employee is Absent");
         }
-        System.out.println("Daily Employee Wage: " + wage);
+        System.out.println("Employee Wage: " + wage);
     }
 }
